f650784b841bc819381e84047eb559cb11065a36
Philipp Oleynik <philipp.oleynik@utu.fi>
2021-05-08 23:07:00 +0300

    Printf initialized

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,15 +18,20 @@
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
+#include <bmm150_common.h>
 #include "main.h"
 #include "i2c.h"
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
+#include "stdio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "retarget.h"
+#include "bmm150.h"
+#include "bmm150_common.h"
+#include "bmm150_defs.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -52,11 +57,98 @@
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
+/*!         Static Function Declaration                                       */
+
+/*!
+ *  @brief This internal API is used to set configurations powermode, odr and interrupt mapping.
+ *
+ *  @param[in] dev       : Structure instance of bmm150_dev.
+ *
+ *  @return Status of execution.
+ */
+static int8_t set_config(struct bmm150_dev *dev);
+
+/*!
+ *  @brief This internal API is used to get gyro data.
+ *
+ *  @param[in] dev       : Structure instance of bmm150_dev.
+ *
+ *  @return Status of execution.
+ */
+static int8_t get_data(struct bmm150_dev *dev);
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
+/*!
+ *  @brief This internal API is used to set configurations like powermode, odr and interrupt mapping.
+ */
+static int8_t set_config(struct bmm150_dev *dev) {
+    /* Status of api are returned to this variable. */
+    int8_t rslt;
+
+    struct bmm150_settings settings;
+
+    /* Set powermode as normal mode */
+    settings.pwr_mode = BMM150_POWERMODE_NORMAL;
+    rslt = bmm150_set_op_mode(&settings, dev);
+    bmm150_error_codes_print_result("bmm150_set_op_mode", rslt);
+
+    if (rslt == BMM150_OK) {
+        /* Setting the preset mode as Low power mode
+         * i.e. data rate = 10Hz, XY-rep = 1, Z-rep = 2
+         */
+        settings.preset_mode = BMM150_PRESETMODE_LOWPOWER;
+        rslt = bmm150_set_presetmode(&settings, dev);
+        bmm150_error_codes_print_result("bmm150_set_presetmode", rslt);
+
+        if (rslt == BMM150_OK) {
+            /* Map the data interrupt pin */
+            settings.int_settings.drdy_pin_en = 0x01;
+            rslt = bmm150_set_sensor_settings(BMM150_SEL_DRDY_PIN_EN, &settings, dev);
+            bmm150_error_codes_print_result("bmm150_set_sensor_settings", rslt);
+        }
+    }
+
+    return rslt;
+}
+
+/*!
+ *  @brief This internal API is used to get gyro data.
+ */
+static int8_t get_data(struct bmm150_dev *dev) {
+    /* Status of api are returned to this variable. */
+    int8_t rslt;
+
+    int8_t idx;
+
+    struct bmm150_mag_data mag_data;
+
+    /* Reading the mag data */
+    while (1) {
+        /* Get the interrupt status */
+        rslt = bmm150_get_interrupt_status(dev);
+
+        if (dev->int_status & BMM150_INT_ASSERTED_DRDY) {
+            printf("Data interrupt occurred\n");
+
+            for (idx = 0; idx < 50; idx++) {
+                /* Read mag data */
+                rslt = bmm150_read_mag_data(&mag_data, dev);
+                bmm150_error_codes_print_result("bmm150_read_mag_data", rslt);
+
+                /* Unit for magnetometer data is microtesla(uT) */
+                printf("MAG DATA[%d]  X : %d uT   Y : %d uT   Z : %d uT\n", idx, mag_data.x, mag_data.y, mag_data.z);
+            }
+        }
+
+        break;
+    }
+
+    return rslt;
+}
 
 /* USER CODE END 0 */
 
@@ -64,99 +156,120 @@ void SystemClock_Config(void);
   * @brief  The application entry point.
   * @retval int
   */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
+int main(void) {
+    /* USER CODE BEGIN 1 */
     char iobuf[64];
 
-  /* USER CODE END 1 */
+    /* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+    /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
 
-  /* USER CODE BEGIN Init */
+    /* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+    /* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+    /* Configure the system clock */
+    SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+    /* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+    /* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_I2C1_Init();
-  MX_TIM11_Init();
-  MX_USART1_UART_Init();
-  /* USER CODE BEGIN 2 */
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_I2C1_Init();
+    MX_TIM11_Init();
+    MX_USART1_UART_Init();
+    /* USER CODE BEGIN 2 */
     HAL_TIM_Base_Start_IT(&htim11);
     HAL_NVIC_EnableIRQ(USART1_IRQn);
     HAL_UART_MspInit(&huart1);
+    RetargetInit(&huart1);
 
-  /* USER CODE END 2 */
+    /* Sensor initialization configuration. */
+    struct bmm150_dev dev;
+    /* Status of api are returned to this variable */
+    int8_t rslt;
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+    rslt = bmm150_interface_selection(&dev);
+    bmm150_error_codes_print_result("bmm150_interface_selection", rslt);
+
+    if (rslt == BMM150_OK) {
+        rslt = bmm150_init(&dev);
+        bmm150_error_codes_print_result("bmm150_init", rslt);
+
+        if (rslt == BMM150_OK) {
+            rslt = set_config(&dev);
+            bmm150_error_codes_print_result("set_config", rslt);
+
+            if (rslt == BMM150_OK) {
+                rslt = get_data(&dev);
+                bmm150_error_codes_print_result("get_data", rslt);
+            }
+        }
+    }
+
+    /* USER CODE END 2 */
+
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "EndlessLoop"
     while (1) {
-    /* USER CODE END WHILE */
+        /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+        /* USER CODE BEGIN 3 */
         if (tick == 0) {
             HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+            printf("HAL!\n");
             tick = 199;
         }
     }
 #pragma clang diagnostic pop
-  /* USER CODE END 3 */
+    /* USER CODE END 3 */
 }
 
 /**
   * @brief System Clock Configuration
   * @retval None
   */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 12;
-  RCC_OscInitStruct.PLL.PLLN = 96;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    /** Configure the main internal regulator output voltage
+    */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    /** Initializes the RCC Oscillators according to the specified parameters
+    * in the RCC_OscInitTypeDef structure.
+    */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 12;
+    RCC_OscInitStruct.PLL.PLLN = 96;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 4;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
+    /** Initializes the CPU, AHB and APB buses clocks
+    */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                                  | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK) {
+        Error_Handler();
+    }
 }
 
 /* USER CODE BEGIN 4 */
@@ -167,14 +280,13 @@ void SystemClock_Config(void)
   * @brief  This function is executed in case of error occurrence.
   * @retval None
   */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
+void Error_Handler(void) {
+    /* USER CODE BEGIN Error_Handler_Debug */
     /* User can add his own implementation to report the HAL error return state */
     __disable_irq();
     while (1) {
     }
-  /* USER CODE END Error_Handler_Debug */
+    /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
